d24e8072e12eca0c91eefbb043a7fd8ea0b198db
Reski Damayanti <dreski080@gmail.com>
2024-11-11 20:00:53 +0700

    faq section completed

--- main.js
+++ main.js
@@ -37,3 +37,26 @@ ScrollReveal().reveal('.header__container .header__flex', {
 	...scrollRevealOption,
 	delay: 1000,
 });
+
+// klik icon arrow in faq section
+const faq = document.querySelector('.faq__grid');
+
+faq.addEventListener('click', e => {
+	const target = e.target;
+	const faqCard = target.closest('.faq__card');
+	if (target.classList.contains('ri-arrow-down-s-line')) {
+		if (faqCard.classList.contains('active')) {
+			faqCard.classList.remove('active');
+		} else {
+			Array.from(faq.children).forEach(item => {
+				item.classList.remove('active');
+			});
+			faqCard.classList.add('active');
+		}
+	}
+});
+
+ScrollReveal().reveal('.faq__image img', {
+	...scrollRevealOption,
+	origin: 'left',
+});
